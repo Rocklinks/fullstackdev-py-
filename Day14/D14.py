94d63cc4f78e33d2de1714023c99aee0ffe2873c
Rocklin K S <rocklin2603@gmail.com>
2025-02-07 21:24:27 +0530

    Create D14.py

---
+++ Day14/D14.py
@@ -0,0 +1,7 @@
+##OOPS
+"""
+Object-Oriented Programming (OOP) is a programming paradigm that organizes
+software design around objects rather than functions and logic. Objects represent real-
+world entities, encapsulating data (attributes) and behavior (methods) into a single
+unit
+"""
